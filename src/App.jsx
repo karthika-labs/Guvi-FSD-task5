abe4aa1ae9f351343483938f92da3cdcdf8e84a5
Karthika K <karthikakarthikeyan0510@gmail.com>
2025-09-22 10:13:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -84,11 +84,7 @@ function App() {
             }
             className="mt-2 text-white hover:text-blue-800 transition duration-500 ease-in-out transform hover:scale-x-110 text-center"
           >
-            {/* <button class=" bona-nova-sc-bold text-[20px]  relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group  border-from-cyan-500 to-blue-500  group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 rounded-md group-hover:bg-transparent group-hover:dark:bg-gray-900">
-                Category
-              </span>
-            </button> */}
+          
             <button type="button" className=" bona-nova-sc-bold text-[20px] text-white bg-gradient-to-r from-blue-700 via-blue-500 to-blue-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Category</button>
           </div>
           {categoryMenu && (
